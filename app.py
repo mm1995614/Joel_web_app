049ea5fd69b37fa60989efc9ffb9ccced9dae82a
mm1995614 <mm1995614@example.com>
2025-09-11 17:20:30 +0800

    加入nginx

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ def index():
             'name': '邊走邊吃',
             'description': '雙人策略棋類遊戲，利用不同大小的棋子達成連線',
             'url': '/move-and-eat',
+            'button_text': '開始遊戲'
+        },
+        {
+            'name': 'BillGets',
+            'description': '記帳分帳小幫手，輕鬆管理群組消費與帳務分攤',
+            'url': '/billgets',
+            'button_text': '開始分帳'
         }
     ]
     return render_template('index.html', games=games)
@@ -26,4 +33,4 @@ def move_and_eat_static(filename):
     return send_from_directory(os.path.join(app.root_path, 'Move_and_Eat', 'static'), filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
